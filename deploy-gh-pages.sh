01fbcfa6947c345f19dad67980a24628b131f84f
Oskar Wickström <oskar.wickstrom@live.com>
2014-08-27 22:28:34 +0200

    Convert makefile shell command

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -4,6 +4,6 @@ git stash || 1
 git checkout gh-pages
 make build-gh-pages
 git add --all -f index.html dist
-(git commit -m "Deploy to gh-pages at $(shell date)" && git rebase master && git push origin +gh-pages) || echo "Nothing has changed!"
+(git commit -m "Deploy to gh-pages at $(date)" && git rebase master && git push origin +gh-pages) || echo "Nothing has changed!"
 git checkout master
 git stash pop
