8269d630da130db7efd4564bb2ce77e421462512
Oskar Wickström <oskar.wickstrom@live.com>
2014-08-27 18:53:01 +0200

    Use PORT env

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ var root = __dirname;
 app.use(express.static(root + '/public'));
 app.use(express.static(root + '/dist'));
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
       console.log('Listening on port %d', server.address().port);
 });
