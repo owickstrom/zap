f0d3df65fa8443ae99444467876ab7a5d2820924
Oskar Wickström <oskar.wickstrom@live.com>
2014-08-28 07:51:10 +0200

    Update deploy script

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -2,7 +2,7 @@
 
 git stash || 1
 git checkout gh-pages
-make build-gh-pages
+make clean index.html
 git add --all -f index.html dist
 (git commit -m "Deploy to gh-pages at $(date)" && git rebase master && git push origin +gh-pages) || echo "Nothing has changed!"
 git checkout master
