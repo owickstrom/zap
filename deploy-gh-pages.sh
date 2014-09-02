89d095025713fd9b7a484fe9d7c04cef7a45e429
Oskar Wickström <oskar.wickstrom@live.com>
2014-09-02 06:21:21 +0200

    Change make command

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -2,7 +2,7 @@
 
 git stash || 1
 git checkout gh-pages
-make clean index.html
+make clean all
 git add --all -f index.html dist
 (git commit -m "Deploy to gh-pages at $(date)" && git rebase master && git push origin +gh-pages) || echo "Nothing has changed!"
 git checkout master
